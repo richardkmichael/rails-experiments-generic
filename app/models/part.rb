4a88cd1e46574f29b1cddde9dda78346e2222503
Richard Michael <rmichael@edgeofthenet.org>
2015-02-26 17:26:25 -0500

    Existence validation.

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,3 +1,17 @@
 class Part < ActiveRecord::Base
   belongs_to :car
+
+  # Simple "existence" validating, this will satisfy NOT NULL and FOREIGN KEY constraints.
+  #
+  #   p = Part.new name: "wheel"
+  #   p.valid?                         #==> false
+  #   p.car = Car.new name: "Toyota"
+  #   p.valid?                         #==> true, despite the car instance (p.car) not yet saved.
+  #   p.save                           #==> Executes "INSERT INTO cars ... ; INSERT INTO parts ... ".
+  #
+  # First, insist a car_id is provided...
+  validates :car, presence: true
+
+  # ... Second, insist the car is valid. This will cause ActiveRecord to `SELECT cars.* WHERE cars.id = car_id`.
+  validates_associated :car
 end
